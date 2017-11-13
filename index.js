e6e13440e73a3831870ae91aeb03c9daff14a4af
Mayank Shah <mayank.24shah@gmail.com>
2017-11-13 01:15:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var katzDeli = [];
+var katzDeliLine = [];
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name);
+    return "Welcome, "+name+"."+" You are number "+ katzDeliLine.length +" in line."
+  }
+
+function nowServing(katzDeliLine){
+let i = 0
+while(katzDeliLine.lenght>i){
+  i++;
+}
+  if(katzDeliLine.length===0){
+    return "There is nobody waiting to be served!"
+}
+ else{
+   return "Currently serving "+katzDeliLine.shift()+".";
+ }
+}
