15b9d2865be3687a4cc3f616035eb945bde30a6d
Mayank Shah <mayank.24shah@gmail.com>
2017-11-13 01:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ while(katzDeliLine.lenght>i){
    return "Currently serving "+katzDeliLine.shift()+".";
  }
 }
+var line = [];
+function currentLine(line){
+  var i=0;
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }
+  else{
+    return
+  }
+}
