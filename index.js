aef8669896d1b6b35a54eb6649b86404d62d165c
Mayank Shah <mayank.24shah@gmail.com>
2017-11-13 01:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,14 @@ while(katzDeliLine.lenght>i){
 var line = [];
 function currentLine(katzDeliLine){
   var i=0;
+  while(katzDeliLine.lenght>i){
+    line.push(''+[i+1]+katzDeliLine[i])
+    i++;
+  }
   if(katzDeliLine.length === 0){
     return "The line is currently empty."
   }
   else{
-    return
+    return line; 
   }
 }
